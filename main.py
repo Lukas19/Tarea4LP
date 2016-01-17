6a23bfbd362a02bf3a449045f8bb4ec9e865009b
WilhelmD <sr.wilzon@gmail.com>
2016-01-17 12:37:57 -0300

    faltan definiciones de procedimientos y arreglar traducciones de IFELESE

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ from stdlib import *
 filename = sys.argv[1]
 text = open(filename)
 traduccion = list()
-indent_level = 0
+indentation = 0
 indent = "    "
 
+traduccion.append("from stdlib import *\n")
 
 def appendline(list, line): #es esto necesario?(\n) quizas deba sacarlo luego de ver lo de las RE.
     newline = line + "\n"
     list.append(newline)
 
 def traducirlinea(line):
-    #if (line calza con RE de proc,asignacion,ifelse,etc):
-    #    traductedline = traduccion de lo que calce.
-    #    newline = indent_level*indent + traductedline
-    #    modificar indent_level segun corresponda.
+
+    indent_level = indentation
+
     if (re.search(r'VAR\((\w+)\) <= (\d+)', line) != None):
         match = re.search(r'VAR\((\w+)\) <= (\d+)', line)
-        linea_traducida =  indent_level*indent + match.group(1) + " = " + match.group(2)
+        linea_traducida =  indent_level[0]*indent + match.group(1) + " = " + match.group(2)
         return linea_traducida
 
-    elif (re.search(r'(\d+) => VAR\((\w+\))') != None):
+    elif (re.search(r'(\d+) => VAR\((\w+\))', line) != None):
         match = re.search(r'(\d+) => VAR\((\w+\))')
         linea_traducida = indent_level*indent + match.group(2) + " = " + match.group(1)
         return linea_traducida
@@ -45,9 +45,56 @@ def traducirlinea(line):
         linea_traducida =  indent_level*indent + match.group(1) + " = False"
         return linea_traducida
 
-    elif (re.search(r'FALSE => VAR\((\w+\))', line) != None):                           #AGREGAR ASIGNACIONES DE PROCEDIMIENTOS Y DE OTRAS VARIABLES!!!!!
+    elif (re.search(r'FALSE => VAR\((\w+\))', line) != None):
         match = re.search(r'FALSE => VAR\((\w+\))', line)
         linea_traducida = indent_level*indent + match.group(1) + " = False"
+        return linea_traducida
+
+    elif (re.search(r'VAR\((\w+)\) <= ([\w\d]+)', line) != None):
+        match = re.search(r'VAR\((\w+)\) <= ([\w\d]+)', line)
+        linea_traducida =  indent_level*indent + match.group(1) + " = " + match.group(2)
+        return linea_traducida
+
+    elif (re.search(r'([\w\d]+) => VAR\((\w+\))', line) != None):
+        match = re.search(r'([\w\d]+) => VAR\((\w+\))')
+        linea_traducida = indent_level*indent + match.group(2) + " = " + match.group(1)
+        return linea_traducida
+
+    elif (re.search(r'VAR\((\w+)\) <= \(([\w\d ]*)\)', line) != None):
+        match = re.search(r'VAR\((\w+)\) <= \(([\w\d ]*)\)', line)
+        args = match.group(2).split()
+        proc = args[0] + "("
+
+        if (len(args) == 1):
+            args.append("")
+
+        for i in range(len(args)):
+            if (i == len(args)-1):
+                proc += args[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc += args[i]+", "
+
+        linea_traducida =  indent_level*indent + match.group(1) + " = " + proc
+        return linea_traducida
+
+    elif (re.search(r'\(([\w\d ]*)\) => VAR\((\w+)\)', line) != None):
+        match = re.search(r'\(([\w\d ]*)\) => VAR\((\w+)\)', line)
+        args = match.group(1).split()
+        proc = args[0] + "("
+
+        if (len(args) == 1):
+            args.append("")
+
+        for i in range(len(args)):
+            if (i == len(args)-1):
+                proc += args[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc += args[i]+", "
+        linea_traducida = indent_level*indent + match.group(2) + " = " + proc
         return linea_traducida
 
     elif (re.search(r'\([\w\d ]*\)', line) != None):
@@ -55,6 +102,9 @@ def traducirlinea(line):
         match = list()
 
         if (len(split) == 1):
+            split.append("")
+
+        if (split[0] == "("):
             linea_traducida = indent_level*indent + "pass"
             return linea_traducida
 
@@ -82,18 +132,146 @@ def traducirlinea(line):
         linea_traducida = indent_level*indent + proc
         return linea_traducida
 
-    elif (re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line) != None): #CORRER ESTE MAS ABAJO DE LAS ASIGNACIONES DE IFELESE!!!!!!
+    elif (re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\) => VAR\((\w+)\)', line) != None):
 
-        match = re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line)
-        statement = ""
+        match = re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\) => VAR\((\w+)\)', line)
         cond = match.group(2)
         splits = cond.split()
+
+        if (len(splits) == 1):
+            splits.append("")
 
         if (cond == "TRUE"):
             cond = "True"
         elif (cond == "FALSE"):
             cond = "False"
-        elif (len(splits) != 1):
+        else:
+            cond = splits[0] + "("
+            for i in range(len(splits)):
+                if (i == len(splits)-1):
+                    cond += splits[i]+")"
+                elif (i == 0):
+                    continue
+                else:
+                    cond += splits[i]+", "
+
+        linea_traducida = indent_level*indent + "if (" + cond + "):\n"
+        indent_level += 1
+        args1 = match.group(1).split()
+        proc1 = args1[0] + "("
+
+        if (len(args1) == 1):
+            args1.append("")
+
+        for i in range(len(args1)):
+            if (i == len(args1)-1):
+                proc1 += args1[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc1 += args1[i]+", "
+
+        linea_traducida += indent_level*indent + match.group(4) + " = " + proc1 + "\n"
+
+        indent_level -= 1
+        linea_traducida += indent_level*indent + "else:\n"
+        indent_level += 1
+        args2 = match.group(3).split()
+        proc2 = args2[0] + "("
+
+        if (len(args2) == 1):
+            args2.append("")
+
+        for i in range(len(args2)):
+            if (i == len(args2)-1):
+                proc2 += args2[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc2 += args2[i]+", "
+        linea_traducida += indent_level*indent + match.group(4) + " = " + proc2
+
+        indent_level -= 1
+
+        return linea_traducida
+
+    elif (re.search(r'VAR\((\w+)\) <= IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line) != None):
+
+        match = re.search(r'VAR\((\w+)\) <= IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line)
+        cond = match.group(3)
+        splits = cond.split()
+
+        if (len(splits) == 1):
+            splits.append("")
+
+        if (cond == "TRUE"):
+            cond = "True"
+        elif (cond == "FALSE"):
+            cond = "False"
+        else:
+            cond = splits[0] + "("
+            for i in range(len(splits)):
+                if (i == len(splits)-1):
+                    cond += splits[i]+")"
+                elif (i == 0):
+                    continue
+                else:
+                    cond += splits[i]+", "
+
+        linea_traducida = indent_level*indent + "if (" + cond + "):\n"
+        indent_level += 1
+        args1 = match.group(2).split()
+        proc1 = args1[0] + "("
+
+        if (len(args1) == 1):
+            args1.append("")
+
+        for i in range(len(args1)):
+            if (i == len(args1)-1):
+                proc1 += args1[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc1 += args1[i]+", "
+
+        linea_traducida += indent_level*indent + match.group(1) + " = " + proc1 + "\n"
+
+        indent_level -= 1
+        linea_traducida += indent_level*indent + "else:\n"
+        indent_level += 1
+        args2 = match.group(4).split()
+        proc2 = args2[0] + "("
+
+        if (len(args2) == 1):
+            args2.append("")
+
+        for i in range(len(args2)):
+            if (i == len(args2)-1):
+                proc2 += args2[i]+")"
+            elif (i == 0):
+                continue
+            else:
+                proc2 += args2[i]+", "
+        linea_traducida += indent_level*indent + match.group(1) + " = " + proc2
+
+        indent_level -= 1
+
+        return linea_traducida
+
+    elif (re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line) != None): #CORRER ESTE MAS ABAJO DE LAS ASIGNACIONES DE IFELESE!!!!!!
+
+        match = re.search(r'IFELSE \(([\w\d ]*)\) \(([\w\d ]*)\) \(([\w\d ]*)\)', line)
+        cond = match.group(2)
+        splits = cond.split()
+
+        if (len(splits) == 1):
+            splits.append("")
+
+        if (cond == "TRUE"):
+            cond = "True"
+        elif (cond == "FALSE"):
+            cond = "False"
+        else:
             cond = splits[0] + "("
             for i in range(len(splits)):
                 if (i == 0):
@@ -108,14 +286,17 @@ def traducirlinea(line):
         args1 = match.group(1).split()
         proc1 = args1[0] + "("
 
-        if (len(proc1) == 1):
+        if (len(args1) == 1):
+            args1.append("")
+
+        if (args1[0] == ""):
             linea_traducida += indent_level*indent + "pass\n"
         else:
-            for i in range(len(proc1)):
-                if (i == 0):
-                    continue
-                elif (i == len(args1)-1):
+            for i in range(len(args1)):
+                if (i == len(args1)-1):
                     proc1 += args1[i]+")"
+                elif (i == 0):
+                    continue
                 else:
                     proc1 += args1[i]+", "
             linea_traducida += indent_level*indent + proc1 + "\n"
@@ -123,23 +304,30 @@ def traducirlinea(line):
         indent_level -= 1
         linea_traducida += indent_level*indent + "else:\n"
         indent_level += 1
-        args2 = match.group(2).split()
+        args2 = match.group(3).split()
         proc2 = args2[0] + "("
 
-        if (len(proc2) == 1):
+        if (len(args2) == 1):
+            args2.append("")
+
+        if (args2[0] == ""):
             linea_traducida += indent_level*indent + "pass\n"
         else:
-            for i in range(len(proc2)):
-                if (i == 0):
-                    continue
-                elif (i == len(args2)-1):
+            for i in range(len(args2)):
+                if (i == len(args2)-1):
                     proc2 += args2[i]+")"
+                elif (i == 0):
+                    continue
                 else:
-                    proc1 += args2[i]+", "
-            linea_traducida += indent_level*indent + proc2 + "\n"
+                    proc2 += args2[i]+", "
+            linea_traducida += indent_level*indent + proc2
 
         indent_level -= 1
 
+        return linea_traducida
+
+    else:
+        linea_traducida = line.strip("\n")
         return linea_traducida
 
 
@@ -152,23 +340,14 @@ for line in text:#recorriendo lineas del archivo de codigo raro
 
 text.close()
 
-newfile = open(filename+".py")
+newfile = open(filename+".py","w")
 
-traduccion.append("from stdlib import *")
 for line in traduccion:
     newfile.write(line)
 
 newfile.close()
 
-#asignaciones de enteros: re.search(r'VAR\(\w+\) <= \d+', string) re.search(r'\d+ => VAR\(\w+\)', string)
-#asignaciones de booleanos: re.search(r'TRUE => VAR\(\w+\)|FALSE => VAR\(\w+\)', string) re.search(r'VAR\(\w+\) <= TRUE|VAR\(\w+\) <= FALSE', string)
-
 
 #Procedimientos: alterar indent_level segun la cantidad de espacios que debe tener al comienzo.
 #Procedimientos: primera linea: def pname(parama):
 #Procedimientos: traducir linea por linea modificando el nivel de indentacion segun corresponda hasta llegar a una liena ^$.
-
-
-#Procedimientos llamadas: pname(list(var1, var2, var3))
-
-#IFELSE: if (cond): <ifproc>  else: <elseproc>. + otras 2 RE para asignacion.
